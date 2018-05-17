c6bc243bff0043ef429bbd7b76bffec1052d1026
Adam Lith <adam.lith@collectorbank.se>
2018-05-17 13:53:48 +0200

    Adding Attributed project, with tests and readme updates.

---
+++ test/Collector.Serilog.Enrichers.SensitiveInformation.Attributed.UnitTest/Helpers/JsonSink.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+
+using Newtonsoft.Json;
+
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+
+namespace Collector.Serilog.Enrichers.SensitiveInformation.UnitTest.Helpers
+{
+    public class JsonSink : ILogEventSink
+    {
+        public string Result { get; set; }
+
+        public string Properties
+        {
+            get
+            {
+                var deserializedObject = JsonConvert.DeserializeObject<OnlyProperties>(Result);
+                return JsonConvert.SerializeObject(deserializedObject.Properties, Formatting.None);
+            }
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            var test = new JsonFormatter();
+            var stringWriter = new StringWriter();
+            test.Format(logEvent, stringWriter);
+
+            Result = stringWriter.ToString();
+        }
+
+        private class OnlyProperties
+        {
+            public object Properties { get; set; }
+        }
+    }
+}
---
+++ test/Collector.Serilog.Enrichers.SensitiveInformation.Attributed.UnitTest/LogAsSensitiveAttribute_Test.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq.Expressions;
+
+using Collector.Serilog.Enrichers.SensitiveInformation.UnitTest.Helpers;
+
+using Destructurama;
+using Destructurama.Attributed;
+
+using Serilog;
+using Serilog.Core;
+
+using Xunit;
+
+namespace Collector.Serilog.Enrichers.SensitiveInformation.Attributed.UnitTest
+{
+    public class LogAsSensitiveAttribute_Test
+    {
+        private JsonSink Sink { get; }
+
+        public LogAsSensitiveAttribute_Test()
+        {
+            Sink = new JsonSink();
+        }
+
+        private Logger CreateLogger()
+        {
+            return new LoggerConfiguration()
+                .Destructure.UsingAttributes()
+                .Enrich.With(new SensitiveInformationEnricher())
+                .WriteTo.Sink(Sink)
+                .CreateLogger();
+        }
+
+        [Fact]
+        public void When_destructuring_a_class_with_attributes()
+        {
+            var logger = CreateLogger();
+
+            logger.ForContext("ContextName", new TestClass(), destructureObjects: true)
+                  .Information("Test");
+
+            Assert.Equal(@"{""ContextName"":{""_typeTag"":""TestClass"",""Prop1"":""Value1""},""__sensitiveInfo"":{""ContextName"":{""_typeTag"":""TestClass"",""Prop2"":""Value2""}}}", Sink.Properties);
+        }
+
+        [Fact]
+        public void When_destructuring_a_class_with_attributes_and_a_given_name()
+        {
+            var logger = CreateLogger();
+
+            logger.ForContext("ContextName", new TestClass2(), destructureObjects: true)
+                  .Information("Test");
+
+            Assert.Equal(@"{""ContextName"":{""_typeTag"":""TestClass2"",""Prop1"":""Value1""},""__sensitiveInfo"":{""ContextName"":{""_typeTag"":""TestClass2"",""Email"":""Value2""}}}", Sink.Properties);
+        }
+
+
+        private class TestClass
+        {
+            public string Prop1 { get; set; } = "Value1";
+
+            [LogAsSensitive]
+            public string Prop2 { get; set; } = "Value2";
+
+            [NotLogged]
+            public string Prop3 { get; set; } = "Value3";
+        }
+
+
+        private class TestClass2
+        {
+            public string Prop1 { get; set; } = "Value1";
+
+            [LogAsSensitive(SensitiveInformationType.Email)]
+            public string Prop2 { get; set; } = "Value2";
+
+            [NotLogged]
+            public string Prop3 { get; set; } = "Value3";
+        }
+    }
+}
